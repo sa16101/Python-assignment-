ce2c1589977b66985c73a50267eb0e1464031430
Shaiba Ali <sa1610166@gmail.com>
2025-08-03 22:16:39 +0530

    Add files via upload

---
+++ assign2.py
@@ -0,0 +1,9 @@
+# Initialize the sum variable
+total_sum = 0
+
+# Use a for loop to iterate from 1 to 50
+for num in range(1, 51):
+    total_sum += num
+
+# Print the result
+print("The sum of integers from 1 to 50 is:", total_sum)
